faa9ee25836927c936689d1cefd7b540052ad574
ethanharman12 <ethanharman12@gmail.com>
2016-07-05 23:47:25 -0400

    Adding MVC website to use Identity

---
+++ Wardrobe.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wardrobe.Web.Startup))]
+namespace Wardrobe.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
