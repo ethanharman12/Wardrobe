0c0960f8f3355dcbfe3eddfa2a5df22b1961225d
ethanharman12 <ethanharman12@gmail.com>
2016-09-10 21:51:36 -0400

    Wiring up Dependency Injection

---
+++ Wardrobe.API/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,76 @@
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Wardrobe.API.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(Wardrobe.API.App_Start.NinjectWebCommon), "Stop")]
+
+namespace Wardrobe.API.App_Start
+{
+    using System;
+    using System.Web;
+
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+    using System.Web.Http;
+    using Wardrobe.Core.Interfaces.Services;
+    using Wardrobe.Core.Services;
+    using Wardrobe.Infrastructure.Repositories;
+    using Wardrobe.Core.Interfaces.Repositories;
+    using Ninject.Web.WebApi;
+
+    public static class NinjectWebCommon 
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start() 
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+        
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+        
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+                RegisterServices(kernel);
+
+                GlobalConfiguration.Configuration.DependencyResolver = new NinjectDependencyResolver(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind<IClosetService>().To<ClosetService>();
+            kernel.Bind<IClosetRepository>().To<ClosetRepository>();
+        }        
+    }
+}
---
+++ Wardrobe.API/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wardrobe.API.Startup))]
+namespace Wardrobe.API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
